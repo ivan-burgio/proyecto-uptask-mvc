e54b5c46e33f8a6c44fe8fc5c3bfc33c970743f2
ivan-burgio <ivan.24.burgio@gmail.com>
2023-07-14 19:11:18 -0300

    Creando la vista para reestablecer

---
+++ views/auth/reestablecer.php
@@ -0,0 +1,26 @@
+<div class="contenedor reestablecer">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Coloca una Contraseña Nueva</p>
+
+        <form action="/reestablecer" class="formulario" method="POST">
+            <div class="campo">
+                <label for="password">Contraseña</label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Tu contraseña"
+                    name="password"
+                >
+            </div>
+
+            <input type="submit" class="boton" value="Guardar contraseña">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes cuenta? Crea una.</a>
+            <a href="/olvide">¿Olvidaste tu contraseña? Cambiala.</a>
+        </div>
+    </div>
+</div>
