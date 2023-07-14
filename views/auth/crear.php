d6d12deffc32eee37be9d2d7bca74799f69f319a
ivan-burgio <ivan.24.burgio@gmail.com>
2023-07-14 18:44:24 -0300

    Creando la vista de crear cuenta

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,57 @@
-<h1>Crear Cuenta</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu Cuenta</p>
+
+        <form action="/" class="formulario" method="POST">
+            
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                    type="text"
+                    id="nombre"
+                    placeholder="Tu nombre"
+                    name="nombre"
+                >
+            </div>
+        
+            <div class="campo">
+                <label for="email">Email</label>
+                <input 
+                    type="email"
+                    id="email"
+                    placeholder="Tu email"
+                    name="email"
+                >
+            </div>
+
+            <div class="campo">
+                <label for="password">Contraseña</label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Tu contraseña"
+                    name="password"
+                >
+            </div>
+
+            <div class="campo">
+                <label for="password2">Repetir contraseña</label>
+                <input 
+                    type="password"
+                    id="password2"
+                    placeholder="Repite tu contraseña"
+                    name="password2"
+                >
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/">¿Ya tienes cuenta? Inicia Sesión.</a>
+            <a href="/olvide">¿Olvidaste tu contraseña? Cambiala.</a>
+        </div>
+    </div>
+</div>
--- views/auth/login.php
+++ views/auth/login.php
@@ -1,6 +1,5 @@
 <div class="contenedor login">
-    <h1 class="uptask">UpTask</h1>
-    <p class="tagline">Crea y Administra tus proyectos</p>
+    <?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
 
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Iniciar Sesión</p>
