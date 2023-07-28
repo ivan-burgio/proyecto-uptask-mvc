47e362acd74ff995f1a248f9b54bbc5439c9449f
ivan-burgio <ivan.24.burgio@gmail.com>
2023-07-28 12:41:13 -0300

    Creando el routing y controladores para tareas

---
+++ controllers/TareaController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+class TareaController {
+    public static function index(Router $router) {
+        
+    }
+
+    public static function crear(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    public static function actualizar(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function eliminar(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 use Controllers\LoginController;
+use Controllers\TareaController;
 use Controllers\DashboardController;
 $router = new Router();
 
@@ -37,7 +38,10 @@ $router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
 
 // API para las tareas
-$router->get('/', [TareasController::class, '']);
+$router->get('/api/tareas', [TareaController::class, 'index']);
+$router->post('/api/tarea', [TareaController::class, 'crear']);
+$router->post('/api/tarea/actualizar', [TareaController::class, 'actualizar']);
+$router->post('/api/tarea/eliminar', [TareaController::class, 'eliminar']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
