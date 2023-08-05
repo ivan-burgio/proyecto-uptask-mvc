a995d6706bbef8b2b27f120b85759439d5daf507
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-05 20:39:42 -0300

    Ocultando paneles para responsive

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,3 +1,10 @@
+<div class="barra-mobile">
+    <h1>UpTask</h1>
+    <div class="menu">
+        <img id="mobile-menu" src="build/img/menu.svg" alt="imagen menu">
+    </div>
+</div>
+
 <div class="barra">
     <p>Bienvendio <span><?php echo $_SESSION['nombre'] ?></span></p>
     <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
