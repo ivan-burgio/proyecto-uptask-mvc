234430da3e5c568eaade23a5b85dcecc84913276
Administrador <ivan.24.burgio@gmail.com>
2023-08-08 12:49:54 -0300

    mostrando y ocultando el menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,20 @@
 const mobileMenuBtn = document.querySelector('#mobile-menu');
+const cerrarMenuBtn =document.querySelector('#cerrar-menu');
 const sidebar = document.querySelector('.sidebar');
 
 if(mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', function() {
-        sidebar.classList.toggle('mostrar');
+        sidebar.classList.add('mostrar');
     });
+}
+
+if(cerrarMenuBtn) {
+    cerrarMenuBtn.addEventListener('click', function() {
+        sidebar.classList.add('ocultar');
+        
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 500);
+    })
 }
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,5 +1,11 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+
+        <div class="cerrar-menu">
+            <img id="cerrar-menu" src="build/img/cerrar.svg" alt="imagen cerrar menu">
+        </div>
+    </div>
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
         <a class="<?php echo ($titulo === 'Crear Proyecto') ? 'activo' : ''; ?>" href="/crear-proyecto">Crear Proyectos</a>
